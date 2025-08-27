a85c3b47ba4007dad40aa7c2e13ee934742646c7
Martin-Rojas <rojas.martin25@yahoo.com>
2025-08-26 19:04:13 -0700

    code for validate country input

--- src/index.js
+++ src/index.js
@@ -10,14 +10,15 @@ const confirmPassword = document.getElementById(`confirm-password`);
 // Validate each field value
 const formInputs = [email, country, postalCode, password, confirmPassword];
 
+// Validate email
 email.addEventListener(`input`, () => {
   email.setCustomValidity(``);
+  country.classList.remove(`invalid`);
+
   if (email.validity.typeMismatch) {
     email.classList.add(`invalid`);
     email.setCustomValidity("I am expecting an email address!");
-  }
-
-  if (email.validity.valueMissing) {
+  } else if (email.validity.valueMissing) {
     email.classList.add(`invalid`);
     email.setCustomValidity("Can't leave blank");
   }
@@ -27,10 +28,35 @@ email.addEventListener(`input`, () => {
   }
 });
 
+// Validate country
+country.addEventListener(`input`, () => {
+  country.setCustomValidity(``);
+  country.classList.remove(`invalid`);
+
+  // Regex: only letters and spaces
+  const regex = /^[A-Za-z\s]+$/;
+
+  if (country.validity.valueMissing) {
+    country.classList.add(`invalid`);
+    country.setCustomValidity("Country is required!");
+  } else if (!regex.test(country.value)) {
+    country.setCustomValidity("Country can only contain letters and spaces.");
+    country.classList.add("invalid");
+  }
+
+  country.reportValidity();
+});
+
 form.addEventListener("submit", (event) => {
+  event.preventDefault();
   if (!email.validity.valid) {
     email.setCustomValidity("Can't leave blank");
     email.reportValidity();
-    event.preventDefault();
+    return;
+  }
+  if (!country.validity.valid) {
+    country.setCustomValidity("Can't leave blank");
+    country.reportValidity();
+    return;
   }
 });
